a49d729cb69926b810898349974865df68eb21c8
Renlord <me@renlord.com>
2020-01-09 16:38:16 +1100

    add verbose logging for otr_transition hook
    Signed-off-by: Renlord <me@renlord.com>

--- contrib/lua/otr.lua
+++ contrib/lua/otr.lua
@@ -1,6 +1,19 @@
 package.path = package.path .. ";/lua/?.lua"
 local gotify
 
+local function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent+1)
+    else
+      print(formatting .. tostring(v))
+    end
+  end
+end
+
 function otr_init()
     local status, _gotify = pcall(require, "gotify")
     if(status) then
@@ -12,9 +25,11 @@ end
 
 function otr_transition(topic, _type, data)
     if (gotify) then
-        gotify.push_message("geofence", data)
+        print(topic.." : ".._type.." : "..tprint(data, nil))
+        m = "User "..data['tid'].." "..data['event'].." "..data['desc'].."."
+        gotify.push_message("geofence", m)
     else
-        print(topic .. " : " .. data)
+        print(topic .. " : " ..tprint(data, nil))
     end
 end
 
